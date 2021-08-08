20814e0abf6aef935cdf6068a23b7254126f26d8
andyjrlee <andyjrlee@outlook.com>
2021-08-08 16:43:51 +0100

    AL: updating code following attempts to fix CORS error - currenly unable to fix issue

--- holidayprojectbackend/src/main/resources/static/location.js
+++ holidayprojectbackend/src/main/resources/static/location.js
@@ -1,4 +1,6 @@
+fetch('http://localhost:9002/cities/read', {mode: 'no-cors'})//
 fetch('http://localhost:9002/cities/read')
+ //fetch('https://reqres.in/api/users?page=2')
 .then((response) => {
     if (response.status !== 200) {
         console.log(`Looks like there was a problem.Status Code: ${ response.status }`);
@@ -8,3 +10,4 @@ response.json()
 .then(data => console.log(data));         
 })
 .catch(err => console.error(`Fetch Error :-S ${err}`));
+
