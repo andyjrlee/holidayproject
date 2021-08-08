f7f5451cbe467aff144bba170f5a30f073c67a11
andyjrlee <andyjrlee@outlook.com>
2021-08-08 20:58:10 +0100

    AL: code update to fetch to show data on webpage

--- holidayprojectbackend/src/main/resources/static/location.js
+++ holidayprojectbackend/src/main/resources/static/location.js
@@ -9,25 +9,25 @@ response.json()
 })
 .catch(err => console.error(`Fetch Error :-S ${err}`));
 
-fetch("http://127.0.0.1:8080/cities/create", { //1
-    method: 'post', //2
-    headers: {
-      "Content-type": "application/json" //3
-    },
-    body: JSON.stringify( //4
-      {
-        "city": "Edinburgh",//5
-        "country": "UK",
-        "hotel_price": "Male",
-        "population": "400000",
-        "recommended_hotel": "McHotel",
-        "region": "East Scotland"
-      }
-    )
-  })
-  .then(res => res.json())
-  .then((data) => console.log(`Request succeeded with JSON response ${data}`))
-  .catch((error) => console.log(`Request failed ${error}`))
+// fetch("http://127.0.0.1:8080/cities/create", { //1
+//     method: 'post', //2
+//     headers: {
+//       "Content-type": "application/json" //3
+//     },
+//     body: JSON.stringify( //4
+//       {
+//         "city": "Edinburgh",//5
+//         "country": "UK",
+//         "hotel_price": "Male",
+//         "population": "400000",
+//         "recommended_hotel": "McHotel",
+//         "region": "East Scotland"
+//       }
+//     )
+//   })
+//   .then(res => res.json())
+//   .then((data) => console.log(`Request succeeded with JSON response ${data}`))
+//   .catch((error) => console.log(`Request failed ${error}`))
 ;
 
 // document.getElementById('id').textContent = id;
@@ -39,7 +39,7 @@ fetch("http://127.0.0.1:8080/cities/create", { //1
    
     // const api_url = 'https://reqres.in/api/users?page=2'
     // async function getABC() {
-    //     const response = await fetch(api_url);
+    //     const response = await fetch(api_url);   
     //     const data = await response.json();
     //     const { id, email } = data;
 
